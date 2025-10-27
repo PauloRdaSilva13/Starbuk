5d002be215c544c45a4c647de779b3df0974b1ee
PauloRdaSilva13 <paullorobsilva@yahoo.com.br>
2025-10-27 18:24:31 -0300

    alteração nos botões das imagens

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let circulo = document.querySelector(".circulo")
+let imagemCopo = document.querySelector(".imagem-copo")
 
 function mudarCor(cor){
 
     circulo.style.backgroundColor = cor
 }
+
+function trocarImagem(imagem){
+    imagemCopo.src = imagem
+}
+
